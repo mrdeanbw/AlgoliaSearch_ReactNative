a52fdf5af76939429986a52c20406ed99adacc58
Lee Solway <lee@digitalanvil.co.uk>
2017-07-10 17:19:41 +0100

    Working addToDate fund

--- functions/utils/addToDate.js
+++ functions/utils/addToDate.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const addToDate = (timestamp, interval, value) => {
+const addToDate = (timestamp, intervalType, value) => {
+  const originalTimestamp = new Date(timestamp);
 
-}
+  switch(intervalType) {
+    case 'd':
+      originalTimestamp.setDate(originalTimestamp.getDate() + value);
+      break;
+    case 'w':
+      const numberOfDays = value * 7
+      originalTimestamp.setDate(originalTimestamp.getDate() + numberOfDays);
+      break;
+    default:
+      throw new Error('Invalid intervalType. Expected d or w but ' + intervalType + ' was given');
+  }
+
+  return originalTimestamp;
+};
 
 module.exports = addToDate;
