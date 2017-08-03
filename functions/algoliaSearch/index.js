97125d9f4a5e3b4d382c236fd3b666a17cf41b21
goodheart7 <peter0901@mail.com>
2017-08-03 19:08:21 +0900

    Add createIndex

---
+++ functions/algoliaSearch/index.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+module.exports = {};
+
+module.exports.initIndex = require('./initIndex');
+module.exports.createIndex = require('./createIndex');
+module.exports.reindexIndex = require('./reindexIndex');
+module.exports.removeIndex = require('./removeIndex');
+module.exports.updateObject = require('./updateObject');
+module.exports.searchQuery = require('./searchQuery');
+
+
